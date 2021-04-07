db4b101a6e64c172980bc45b8443b50bb187beeb
Gard.Kalland <gard.kalland@student.uib.no>
2021-04-08 00:24:30 +0200

    hyperlink shitt

--- FMI/webserver/web-server.py
+++ FMI/webserver/web-server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 from station import weather as w
 from station import structuring as s
+
 
 app = Flask(__name__)
 
@@ -30,11 +31,18 @@ def karmøy():
 
 
 
-@app.route('/Day')
+@app.route('/day')
 def Day():
-    return render_template('day.html', weather=w.bergentemps)
-
-
+    req = request.args.get('day')
+    day = int(req[0])
+    loc = req
+    print(loc)
+    if 'Bergen' in loc:
+        return render_template('day.html', weather=w.bergentemps, days=int(day))
+    elif 'Karmøy' in loc:
+        return render_template('day.html', weather=w.karmøytemps, days=int(day))
+    elif 'Oslo' in loc:
+        return render_template('day.html', weather=w.Oslo, days=int(day))
 
 if __name__ == '__main__':
     app.run(debug=True)
