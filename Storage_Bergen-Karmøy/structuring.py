d48ae46bda4f54b111c7639d4f65746fe688bea1
Osten.Edvardsen <osten.edvardsen@student.uib.no>
2021-04-08 13:39:57 +0200

    Structuring of averages

---
+++ Storage_Bergen-Karmøy/structuring.py
@@ -0,0 +1,26 @@
+dailySumTemp = 0
+dailyAverageTemp = 0
+
+dailySumPrep = 0
+dailyAveragePrep = 0
+hourCount = 0
+
+dagVar = []
+snitt = []
+
+def avg_temp(locationData):
+    for item in locationData:
+        dailySumTemp += item[4]
+        dailySumPrep += item[5]
+            if hourCount == 23:
+                dailyAverageTemp = dailySumTemp/24
+                dailyAverageTemp = round(dailyAverageTemp, 2)
+                
+                dailyAveragePrep = dailySumPrep/24
+                dailyAveragePrep = round(dailyAveragePrep, 2)
+
+                snitt.append([item[1], item[2], item[3], dailyAveragePrep, dailyAveragePrep])
+
+                hourCount = 0
+
+    return snitt
